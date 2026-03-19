af2db3d76557efc3cf843b5eb7314c7245cb3bbf
sergioramirez06 <sergio200206mayo@gmail.com>
2026-03-19 15:36:50 -0500

    Verificando nodemailer

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,11 @@ import { Server } from './presentation/server';
 
 function main() {
   const port = Number(process.env.PORT ?? 3000);
+  console.log('MAILER_SERVICE',process.env.MAILER_SERVICE)
+  console.log('MAILER_EMAIL',process.env.MAILER_EMAIL)
+  console.log('MAILER_SECRET_KEY',process.env.MAILER_SECRET_KEY)
+  console.log('SEND_EMAIL', process.env.SEND_EMAIL)
+
 
   const server = new Server({
     port,
